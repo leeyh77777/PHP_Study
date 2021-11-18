139dfe6b8a7aa3ea5aa57821d3ef8c4506f31087
leeyh777 <82528233+leeyh777@users.noreply.github.com>
2021-11-19 02:41:24 +0900

    21.11.18

---
+++ ex_array2.php
@@ -0,0 +1,18 @@
+<?php
+$fruits = [];
+$fruits[0] = "apple";
+$fruits[1] = "mango";
+$fruits[] = "melon"; // 마지막 배열 끝에 추가 , 기본값 melon2로 변경
+$fruits['key1'] = "value1";
+$fruits[] = "peach";
+
+$fruits['key1'] = "value2";
+$fruits[2] = "melon2"; // melon 에 대체 
+array_push($fruits, "a", "b", "c", "d"); // 추가
+
+unset($fruits['key1']); // 삭제 
+
+
+echo "<pre>";
+print_r($fruits);
+echo "</pre>";
