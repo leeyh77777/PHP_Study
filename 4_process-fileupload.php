1d36c0117b38e16c27a2e9cbb570c13862ce5adf
leeyh77777 <86813278+leeyh77777@users.noreply.github.com>
2021-12-06 00:35:53 +0900

    Update 4_process-fileupload.php

--- 4_process-fileupload.php
+++ 4_process-fileupload.php
@@ -3,3 +3,17 @@ echo "<pre>";
 print_r($_FILES);
 echo "<pre>";
 move_uploaded_file($_FILES['file']['tmp_name'], "upload/".$_FILES['file']['name']);
+/** result
+$_FILES : 
+Array
+(
+    [file] => Array
+        (
+            [name] => logo.png
+            [type] => image/png
+            [tmp_name] => /tmp/phpY6Armu
+            [error] => 0
+            [size] => 291733
+        )
+)
+*/
